a452a7b1db8fe771d4ee850e19b12f18a4ce2173
beechan <hisobe@netprotections.co.jp>
2016-06-21 06:41:09 +0000

    ビュー作成/細かいところをパーシャルで切り分け

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,33 @@
 class StaticPagesController < ApplicationController
   def home
+    @schedule = Schedule.new
+    @schedules = Schedule.all
   end
+  
+  def edit
+    @schedule = Schedule.find(params[:id])
+  end
+  
+  def destroy
+    @schedule.destroy
+    redirect_to root_path, notice: 'ひま時間を削除しました'
+  end
+  
+  def update
+    if @schedule.update(schedule_params)
+      redirect_to root_path , notice: 'ひま時間を編集しました'
+    else
+      render 'edit'
+    end
+  end
+
+  private
+  def schedule_params
+    params.require(:schedule).permit(:starting_time, :end_time)
+  end
+  
+  def set_schedule
+    @schedule = Schedule.find(params[:id])
+  end
+
 end
